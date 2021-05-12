aa0ec414243a10d025b5996651468cd069758029
Szymon Fiedler <szymon.fiedler2@gmail.com>
2021-05-12 09:36:47 +0200

    Initialized spring backend application

---
+++ tictactoe-backend/src/test/java/com/vm/interview/tictactoe/TicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vm.interview.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
